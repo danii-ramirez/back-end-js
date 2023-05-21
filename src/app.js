24279e814d34f967ed8c3599184233870b863d0b
Daniel Ramirez <ramirezdani016@hotmail.com>
2023-05-21 18:53:51 -0300

    entrega servidor express

--- src/app.js
+++ src/app.js
@@ -1,19 +1,29 @@
 const express = require('express')
+const ProductManager = require('./productManager')
 
 const app = express()
 
-app.get('/products', (req, res) => {
-    let limit = req.query
+app.get('/products', async (req, res) => {
+    let { limit } = req.query
 
-    res.send('express')
+    const productManager = new ProductManager()
+    let products = await productManager.getProducts()
+
+    if (limit !== undefined) {
+        products = products.splice(0, limit)
+    }
+
+    res.send({ products: products })
 })
 
-app.get('/product/:pid', (req, res) => {
-    let idProduct = req.params.pid
+app.get('/product/:id', async (req, res) => {
+    let idProduct = req.params.id
 
-    res.send(idProduct)
+    const productManager = new ProductManager()
+    let product = await productManager.getProductById(idProduct)
+    res.send(product)
 })
 
 app.listen(8080, () => {
-    console.log('listening port 8080')
+    console.log('server runnig port 8080')
 })
--- src/productManager.js
+++ src/productManager.js
@@ -1,2 +1,30 @@
 const fs = require('fs')
 
+class ProductManager {
+    constructor() {
+        this.path = __dirname + '/products.json'
+    }
+
+    async getProducts() {
+        try {
+            let file = await fs.promises.readFile(this.path, 'utf-8')
+            let products = JSON.parse(file)
+            return products
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    async getProductById(id) {
+        try {
+            let file = await fs.promises.readFile(this.path, 'utf-8')
+            const products = JSON.parse(file)
+            let product = products.find(x => x.id == id)
+            return product
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+module.exports = ProductManager
